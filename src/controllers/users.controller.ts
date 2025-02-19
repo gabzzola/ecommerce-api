f6f3cf6a91cf846cf7b037b6ae7843ca198d3391
gabzzola <gazzola.co@gmail.com>
2025-02-19 00:44:54 -0300

    Implementar o middleware express-async-handler

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,6 +1,6 @@
-import { NextFunction, Request, Response } from "express";
-import { NotFound } from "../errors/not-found.error";
+import { Request, Response } from "express";
 import { getFirestore } from "firebase-admin/firestore";
+import { NotFound } from "../errors/not-found.error";
 import { ValidationError } from "../errors/validation.error";
 
 type User = {
@@ -10,95 +10,75 @@ type User = {
 
 export class UserController {
 
-    static async getAll(req: Request, res: Response, next: NextFunction) {
-        try {
-            const snapshot = await getFirestore().collection("users").get();
+    static async getAll(req: Request, res: Response) {
+        const snapshot = await getFirestore().collection("users").get();
 
-            if (snapshot.empty) {
-                throw new NotFound("Nenhum usuário encontrado.");
-            };
+        if (snapshot.empty) {
+            throw new NotFound("Nenhum usuário encontrado.");
+        };
 
-            const users = snapshot.docs.map(doc => {
-                return {
-                    id: doc.id,
-                    ...doc.data()
-                };
-            });
-            res.send(users);
-        } catch (error) {
-            next(error);
-        }
-    }
-
-    static async getById(req: Request, res: Response, next: NextFunction) {
-        try {
-            const userId = req.params.id;
-            const doc = await getFirestore().collection("users").doc(userId).get();
-            
-            if (!doc.exists) {
-                throw new NotFound(`Não há nenhum usuário cadastrado com o ID ${userId}.`)
-            };
-
-            res.send({
+        const users = snapshot.docs.map(doc => {
+            return {
                 id: doc.id,
                 ...doc.data()
-            });
-        } catch (error) {
-            next(error);
-        }
-    }
-
-    static async save(req: Request, res: Response, next: NextFunction) {
-        try {            
-            const user = req.body as User;
-
-            if (!user.name) {
-                throw new ValidationError("O nome é obrigatório!");
             };
-            if (!user.email) {
-                throw new ValidationError("O email é obrigatório!");
-            };
-            
-            const savedUser = await getFirestore().collection("users").add(user);
-            res.status(201).send({
-                message: `Usuário ${savedUser.id} criado com sucesso!`
-            });
-        } catch (error) {
-            next(error);
-        }
+        });
+        res.send(users);
     }
 
-    static async update(req: Request, res: Response, next: NextFunction) {
-        try {
-            const userId = req.params.id;
-            const user = req.body as User;
-            const docRef = getFirestore().collection("users").doc(userId);
-    
-            const doc = await docRef.get();
-            if (!doc.exists) {
-                throw new NotFound(`Não há nenhum usuário cadastrado com o ID ${userId}.`);
-            }
-    
-            await docRef.update({
-                name: user.name,
-                email: user.email
-            });
-    
-            res.send({
-                message: `Usuário ${userId} alterado com sucesso!`,            
-            });
-        } catch (error) {
-            next(error);
-        }
+    static async getById(req: Request, res: Response) {
+        const userId = req.params.id;
+        const doc = await getFirestore().collection("users").doc(userId).get();
+        
+        if (!doc.exists) {
+            throw new NotFound(`Não há nenhum usuário cadastrado com o ID ${userId}.`)
+        };
+
+        res.send({
+            id: doc.id,
+            ...doc.data()
+        });
     }
 
-    static async delete(req: Request, res: Response, next: NextFunction) {
-        try {
-            const userId = req.params.id;
-            await getFirestore().collection("users").doc(userId).delete();
-            res.status(204).end();
-        } catch (error) {
-            next(error);
+    static async save(req: Request, res: Response) {
+        const user = req.body as User;
+
+        if (!user.name) {
+            throw new ValidationError("O nome é obrigatório!");
+        };
+        if (!user.email) {
+            throw new ValidationError("O email é obrigatório!");
+        };
+        
+        const savedUser = await getFirestore().collection("users").add(user);
+        res.status(201).send({
+            message: `Usuário ${savedUser.id} criado com sucesso!`
+        });
+    }
+
+    static async update(req: Request, res: Response) {
+        const userId = req.params.id;
+        const user = req.body as User;
+        const docRef = getFirestore().collection("users").doc(userId);
+
+        const doc = await docRef.get();
+        if (!doc.exists) {
+            throw new NotFound(`Não há nenhum usuário cadastrado com o ID ${userId}.`);
         }
+
+        await docRef.update({
+            name: user.name,
+            email: user.email
+        });
+
+        res.send({
+            message: `Usuário ${userId} alterado com sucesso!`,            
+        });
+    }
+
+    static async delete(req: Request, res: Response) {
+        const userId = req.params.id;
+        await getFirestore().collection("users").doc(userId).delete();
+        res.status(204).end();
     }
 }
